802a9f401ef8a5f351ab50e10440d7c1a4e889bb
Jayson H Bucy <jayson.h.b@gmail.com>
2017-01-25 10:32:57 -0600

    Checkpoint 29 Comment functionality added

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,13 @@ Rails.application.routes.draw do
     resources :posts, except: [:index]
   end
 
+  resources :posts, only: [] do
+    resources :comments, only: [:create, :destroy]
+  end
+
   resources :users, only: [:new, :create]
   resources :sessions, only: [:new, :create, :destroy]
-  
+
   get 'about' => 'welcome#about'
   get 'contact' => 'wecome#contact'
   get 'faq' => 'welcome#faq'
